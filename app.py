1de3c8f44b9259fcaca6ab374b3b68a43e7e516e
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2023-10-02 09:46:40 +0530

    title changed

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ in_prompt = gradio.components.Textbox(lines=10, placeholder=None, label='Enter r
 out_response = gradio.components.Textbox(type="text", label='Sentiment')
 
 # Gradio interface to generate UI link
-title = "Sentiment Classification"
+title = "Sentiment Classification Test"
 description = "Analyse sentiment of the given review"
 
 iface = gradio.Interface(fn = predict_sentiment,
